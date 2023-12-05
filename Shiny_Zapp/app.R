bc861cd64843f8c8d10a4aa7812d0f5bb3272144
ergeis <ergeis@umich.edu>
2023-12-04 19:27:40 -0500

    updated shiny app- the plots aren't working quite right yet

--- Shiny_Zapp/app.R
+++ Shiny_Zapp/app.R
@@ -1,24 +1,53 @@
+install.packages("shiny")
+install.packages("MASS")
+library(shiny)
+library(MASS)
+Final <- read.csv("finalModelData.csv")
+
+
+
+
 
 
 main_page <- tabPanel(
   title = "Analysis",
-  titlePanel = ("Analysis"),
+  titlePanel("Analysis"),
   sidebarLayout(
     sidebarPanel(
+      title = "Inputs",
       selectInput('Outcome', 'Select the outcome variable', choices = names(Final)),
       selectizeInput('Predictors', 'Select the predictor variable/s', choices = names(Final), multiple=TRUE),
       actionButton("run_regression", "Run Regression")
-    )),
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Model", verbatimTextOutput("model_summary")),
-      tabPanel("Residual Plots", plotOutput("plot"))
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel(
+          title = "Model", verbatimTextOutput("model_summary")
+        ),
+        tabPanel(
+          title = "Residual Plots", plotOutput("plot")
+        )
+      )
     )
   )
 )
 
+
+
 plots_page <- tabPanel(
-  title = "Exploratory Plots"
+  title = "Exploratory Plots",
+  titlePanel("Exploratory Plots"),
+  sidebarLayout(
+    sidebarPanel(
+      title = "Inputs",
+      selectInput('x_axis', 'Select the x variable', choices = names(Final)),
+      selectizeInput('y_axis', 'Select the y variable', choices = names(Final)),
+      actionButton("create_plot", "Create Plot")
+    ),
+    mainPanel(
+      title = "Plot", plotOutput("exp_plot")
+    )
+  )
 )
 
 
@@ -28,17 +57,9 @@ server <- function(input, output) {
     outcome <- input$Outcome
 
     #run the regression model
-
     model <- glm.nb(as.formula(paste(outcome, "~", paste(predictors, collapse = "+"), "+", "offset(log(Final$Population))")),
                     link = log,
                     data = Final)
-
-    output$scatterplot <- renderPlot({
-      ggplot(Final, aes_string(x = Predictors, y = Outcome)) +
-        geom_point() +
-        geom_smooth(method = "lm", se = FALSE, color = "blue") +
-        labs(title = "HOT SNAKS")
-    })
 
     output$model_summary <- renderPrint({
       summary(model)
@@ -46,6 +67,18 @@ server <- function(input, output) {
     output$plot <- renderPlot({
       plot(fitted(model), resid(model))
     })
+  })
+  observeEvent(input$create_plot, {
+      x_var <- input$x_axis
+      y_var <- input$y_axis
+
+      #create plot
+      output$exp_plot <- renderPlot({
+        plot(Final[[x_var]],Final[[y_var]],
+             xlim = range(Final[[x_var]], na.rm = TRUE),
+             ylim = range(Final[[y_var]], na.rm = TRUE))
+      }
+      )
   })
 }
 
